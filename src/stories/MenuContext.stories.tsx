0715781d5cee975a1525ad4cbcd61fbaeff61dde
Aleksei Palenyi <alexx384@yandex.ru>
2023-03-29 09:34:27 +0200

    Add story documentation based on https://corechasm.com/docs/vue/writing-docs/doc-blocks

--- src/stories/MenuContext.stories.tsx
+++ src/stories/MenuContext.stories.tsx
@@ -4,14 +4,23 @@ import { MenuContext } from '../components/MenuContext';
 export default {
   title: 'Stories/MenuContext',
   component: MenuContext,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'We can invoke context menu on any component inside **MenuContext** block. Try it by clicking on the _Hello World_ using your the right mouse click.',
+      },
+    },
+  },
 } as ComponentMeta<typeof MenuContext>;
 
 const Template: ComponentStory<typeof MenuContext> = (args) => (
-  <MenuContext {...args} />
+  <MenuContext {...args}>
+    <h1 style={{ color: '#ffffff' }}>Hello World</h1>
+  </MenuContext>
 );
 
 export const PrimarySelect = Template.bind({});
 PrimarySelect.args = {
-  children: <h1>Hello World</h1>,
   menuItems: ['Edit', 'Delete'],
 };
