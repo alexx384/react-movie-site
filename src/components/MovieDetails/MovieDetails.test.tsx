10bae80d5e9ff683cf754fc6571baf67d7f62daf
Aleksei Palenyi <alexx384@yandex.ru>
2023-03-29 14:09:35 +0200

    Add test for MovieDetails based on snapshot testing

--- src/components/MovieDetails/MovieDetails.test.tsx
+++ src/components/MovieDetails/MovieDetails.test.tsx
@@ -1,19 +1,20 @@
-import { render, screen } from '@testing-library/react';
 import { MovieDetails } from './MovieDetails';
+import renderer from 'react-test-renderer';
 
-it('renders an input with the value equal to initial value passed in props', () => {
-  render(
-    <MovieDetails
-      imageUrl="logo192.png"
-      movieName="React"
-      releaseYear={2022}
-      rating={8.9}
-      genres={['Adventure']}
-      durationInSeconds={9240}
-      description="Whether you work on your own or with thousands of other developers, using React feels the same. It is designed to let you seamlessly combine components written by independent people, teams, and organizations."
-    />
-  );
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <MovieDetails
+        imageUrl="logo192.png"
+        movieName="React"
+        releaseYear={2022}
+        rating={8.9}
+        genres={['Adventure']}
+        durationInSeconds={9240}
+        description="Whether you work on your own or with thousands of other developers, using React feels the same. It is designed to let you seamlessly combine components written by independent people, teams, and organizations."
+      />
+    )
+    .toJSON();
 
-  // const searchInput: HTMLElement = screen.getByRole('textbox');
-  // expect(searchInput).toHaveValue(initialSearchQuery);
+  expect(tree).toMatchSnapshot();
 });
