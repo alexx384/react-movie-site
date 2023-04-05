e89f0df406648bdf858d03e5aaccac8596fb6c84
Aleksei Palenyi <alexx384@yandex.ru>
2023-04-05 10:56:04 +0200

    Fix Dialog trap

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -1,9 +1,10 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
-import { Dialog } from '.';
+import { Dialog, DialogHandle } from '.';
 import fontStyles from '../../Font.module.css';
 import searchFormStyles from '../SearchForm/SearchForm.module.css';
 import classNames from 'classnames';
+import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Stories/Dialog',
@@ -11,27 +12,28 @@ export default {
 } as ComponentMeta<typeof Dialog>;
 
 const Template: ComponentStory<typeof Dialog> = (args) => {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const dialogRef = React.useRef<DialogHandle>(null);
+  function handleOpenModalDialog() {
+    dialogRef.current?.showDialog();
+  }
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Click to open modal</button>
-      {isOpen && (
-        <Dialog {...args} onClose={() => setIsOpen(false)}>
-          <p style={{ margin: '0 0 3rem 0' }}>
-            Are you sure you want to delete this movie?
-          </p>
-          <div style={{ textAlign: 'right' }}>
-            <button
-              className={classNames(
-                fontStyles['submit-btn'],
-                searchFormStyles['submit-btn']
-              )}
-            >
-              CONFIRM
-            </button>
-          </div>
-        </Dialog>
-      )}
+      <button onClick={handleOpenModalDialog}>Click to open modal</button>
+      <Dialog ref={dialogRef} {...args} onClose={action('onClose')}>
+        <p style={{ margin: '0 0 3rem 0' }}>
+          Are you sure you want to delete this movie?
+        </p>
+        <div style={{ textAlign: 'right' }}>
+          <button
+            className={classNames(
+              fontStyles['submit-btn'],
+              searchFormStyles['submit-btn']
+            )}
+          >
+            CONFIRM
+          </button>
+        </div>
+      </Dialog>
     </>
   );
 };
--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,40 +1,61 @@
 import React from 'react';
-import { createPortal } from 'react-dom';
+import { Portal } from './Portal';
 import styles from './Dialog.module.css';
 import fontStyles from '../../Font.module.css';
 import classNames from 'classnames';
 import FocusTrap from 'focus-trap-react';
 import { DIALOG_X_BUTTON } from '../../constants/tests.constants';
 
+export type DialogHandle = {
+  showDialog: () => void;
+};
+
 type Props = {
   title: string;
   children: React.ReactNode;
+  isInitiallyShowed?: boolean;
   onClose?: () => void;
 };
 
-export const Dialog = ({ title, children, onClose }: Props) => {
-  function handleCloseClick() {
-    onClose?.();
+export const Dialog = React.forwardRef<DialogHandle, Props>(
+  ({ title, children, onClose, isInitiallyShowed = true }, ref) => {
+    const [isShowTrap, setIsShowTrap] = React.useState(isInitiallyShowed);
+    React.useImperativeHandle(ref, () => ({
+      showDialog: () => {
+        setIsShowTrap(true);
+      },
+    }));
+    function handleDeactivate() {
+      setIsShowTrap(false);
+    }
+    function handleCloseClick() {
+      setIsShowTrap(false);
+      onClose?.();
+    }
+    return (
+      <>
+        {isShowTrap && (
+          <Portal container={document.body}>
+            <FocusTrap focusTrapOptions={{ onDeactivate: handleCloseClick }}>
+              <div className={styles.modal}>
+                <div className={styles.block}>
+                  <button
+                    className={styles['close-btn']}
+                    onClick={handleDeactivate}
+                    data-testid={DIALOG_X_BUTTON}
+                  >
+                    🗙
+                  </button>
+                  <h1 className={classNames(fontStyles.title, styles.title)}>
+                    {title}
+                  </h1>
+                  <div className={styles.body}>{children}</div>
+                </div>
+              </div>
+            </FocusTrap>
+          </Portal>
+        )}
+      </>
+    );
   }
-
-  return createPortal(
-    <FocusTrap focusTrapOptions={{ onDeactivate: handleCloseClick }}>
-      <div className={styles.modal}>
-        <div className={styles.block}>
-          <button
-            className={styles['close-btn']}
-            onClick={handleCloseClick}
-            data-testid={DIALOG_X_BUTTON}
-          >
-            🗙
-          </button>
-          <h1 className={classNames(fontStyles.title, styles.title)}>
-            {title}
-          </h1>
-          <div className={styles.body}>{children}</div>
-        </div>
-      </div>
-    </FocusTrap>,
-    document.body
-  );
-};
+);
