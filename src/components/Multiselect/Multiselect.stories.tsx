fde341ca150980ad25f799fb1542ba89aeaa1df9
Aleksei Palenyi <alexx384@yandex.ru>
2023-05-16 15:25:31 +0200

    Fix multiselect property bug Fix undefined totalMovieListNumber

--- src/components/Multiselect/Multiselect.stories.tsx
+++ src/components/Multiselect/Multiselect.stories.tsx
@@ -1,15 +1,28 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Multiselect } from '.';
 import { DEFAULT_MOVIE_GENRES } from '../../constants/movieForm.constants';
+import React from 'react';
 
 export default {
   title: 'Stories/Multiselect',
   component: Multiselect,
 } as ComponentMeta<typeof Multiselect>;
 
-const Template: ComponentStory<typeof Multiselect> = (args) => (
-  <Multiselect {...args} />
-);
+const Template: ComponentStory<typeof Multiselect> = (args) => {
+  const [selectedOptions, setSelectedOptions] = React.useState(
+    new Set<string>()
+  );
+  const handleMultiselectChange = (newSelectedOptions: Set<string>) => {
+    setSelectedOptions(new Set(newSelectedOptions));
+  };
+  return (
+    <Multiselect
+      {...args}
+      initiallySelectedOptions={selectedOptions}
+      onChange={handleMultiselectChange}
+    />
+  );
+};
 
 export const PrimaryMovieForm = Template.bind({});
 PrimaryMovieForm.args = {
